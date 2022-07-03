b432325dc3bcbf0f27f27db6f00ca9abf4903895
Fatih <fatihelci@gmail.com>
2022-07-03 18:49:21 +0300

    Add proc

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager()
